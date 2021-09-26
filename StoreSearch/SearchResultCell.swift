8ab4c13ed9a4dd727f353cff28d895767bb5f095
laned3 <89052920+laned3@users.noreply.github.com>
2021-09-26 12:28:59 -0400

    Add files via upload

---
+++ StoreSearch/SearchResultCell.swift
@@ -0,0 +1,49 @@
+//
+//  SearchResultCell.swift
+//  StoreSearch
+//
+//  Created by Lane Dever on 9/26/21.
+//
+
+import UIKit
+
+class SearchResultCell: UITableViewCell {
+  @IBOutlet var nameLabel: UILabel!
+  @IBOutlet var artistNameLabel: UILabel!
+  @IBOutlet var artworkImageView: UIImageView!
+
+  var downloadTask: URLSessionDownloadTask?
+
+  override func awakeFromNib() {
+    super.awakeFromNib()
+    let selectedView = UIView(frame: CGRect.zero)
+    selectedView.backgroundColor = UIColor(named: "SearchBar")?.withAlphaComponent(0.5)
+    selectedBackgroundView = selectedView
+  }
+
+  override func prepareForReuse() {
+    super.prepareForReuse()
+    downloadTask?.cancel()
+    downloadTask = nil
+  }
+
+  override func setSelected(_ selected: Bool, animated: Bool) {
+    super.setSelected(selected, animated: animated)
+    // Configure the view for the selected state
+  }
+
+  // MARK: - Helper Methods
+  func configure(for result: SearchResult) {
+    nameLabel.text = result.name
+
+    if result.artist.isEmpty {
+      artistNameLabel.text = "Unknown"
+    } else {
+      artistNameLabel.text = String(format: "%@ (%@)", result.artist, result.type)
+    }
+    artworkImageView.image = UIImage(systemName: "square")
+    if let smallURL = URL(string: result.imageSmall) {
+      downloadTask = artworkImageView.loadImage(url: smallURL)
+    }
+  }
+}
---
+++ StoreSearch/SearchResults.swift
@@ -0,0 +1,13 @@
+//
+//  SearchResults.swift
+//  StoreSearch
+//
+//  Created by Lane Dever on 9/25/21.
+//
+
+import Foundation
+
+class SearchResult {
+  var name = ""
+  var artistName = ""
+}
